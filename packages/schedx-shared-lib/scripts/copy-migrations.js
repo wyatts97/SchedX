78a1b2d6d0ef35f75014061dffa5d1edbc738c5a
wyatts97 <wyattlonerboy97@gmail.com>
2025-10-11 18:47:30 -0500

    Update

--- packages/schedx-shared-lib/scripts/copy-migrations.js
+++ packages/schedx-shared-lib/scripts/copy-migrations.js
@@ -1,4 +1,4 @@
-import { cpSync } from 'fs';
+import { cpSync, mkdirSync } from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
@@ -9,5 +9,6 @@ const srcDir = join(__dirname, '..', 'src', 'backend', 'migrations');
 const destDir = join(__dirname, '..', 'dist', 'backend', 'migrations');
 
 console.log('Copying migrations from', srcDir, 'to', destDir);
+mkdirSync(destDir, { recursive: true });
 cpSync(srcDir, destDir, { recursive: true });
 console.log('Migrations copied successfully');
